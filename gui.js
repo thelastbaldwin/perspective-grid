68a86605c902def93c3a2ab3356e1f5b2c40fdc1
Steve Minor <stv.mnr@gmail.com>
2018-03-16 10:57:05 -0700

    vanishing points now fixed at 90 degrees

--- gui.js
+++ gui.js
@@ -1,19 +1,18 @@
 const gui = new dat.GUI();
 
 const controls = new function(){
+    this.FOV = 60;
     this.horizon = .5;
-    this.leftVanishingPoint = -.1;
-    this.leftDecay = .98;
-    this.rightVanishingPoint = 1.1;
-    this.rightDecay = .98;
+    this.lvpAngle = 45;
+    this.rvpAngle = 45;
     this.lineCount = 60;
     this.lineThickness = .01;
 }
 
+gui.add(controls, "FOV", 50, 90);
+gui.add(controls, "rvpAngle", 0, 90).step(1);
+const lvpAngle = gui.add(controls, "lvpAngle", 0, 90).listen();
 gui.add(controls, "horizon", 0, 1);
 gui.add(controls, "lineCount", 36, 160);
-gui.add(controls, "leftVanishingPoint", -1, 2);
-gui.add(controls, "rightVanishingPoint", -1, 2);
 gui.add(controls, "lineThickness", .01, .99);
-
 gui.width = 400;
--- perspectiveGrid.js
+++ perspectiveGrid.js
@@ -12,6 +12,9 @@ const quadMaterial = new THREE.ShaderMaterial({
         u_resolution:{
             value: new THREE.Vector2(window.innerWidth, window.innerHeight)
         },
+        u_FOV:{
+            value: controls.FOV
+        },
         u_hl: {value: controls.horizon},
         u_lineCount: {value: controls.lineCount},
         u_lvp: {value: controls.leftVanishingPoint},
@@ -29,6 +32,22 @@ camera.position.z = 100;
 camera.lookAt(scene.position);
 scene.add(camera);
 scene.add(quad);
+
+function degToRad(deg){
+    return deg * Math.PI / 180;
+}
+
+
+// TODO: use this relative to the longest edge
+// returns the depth of the station point as a ratio of the FOV to longest edge
+// assumes that the FOV is length of the longest edge
+function getSPLength(FOVrad){
+    return .5 / Math.tan(FOVrad/2);
+}
+
+function getVPDistanceFromCenter(sp, angle){
+    return sp * Math.tan(angle);
+}
 
 function updateAspectRatioDisplay(){
     document.querySelector("#info span").innerText = (window.innerWidth / window.innerHeight).toFixed(2);
@@ -51,17 +70,39 @@ function initScene(){
     render();
 }
 
-function updateUniforms(){
-    quadMaterial.uniforms.u_resolution.value = new THREE.Vector2(window.innerWidth, window.innerHeight);
-    quadMaterial.uniforms.u_hl.value = controls.horizon;
-    quadMaterial.uniforms.u_lineCount.value = controls.lineCount;
-    quadMaterial.uniforms.u_lvp.value = controls.leftVanishingPoint;
-    quadMaterial.uniforms.u_rvp.value = controls.rightVanishingPoint;
-    quadMaterial.uniforms.u_lineThickness.value = controls.lineThickness;
+function setLVPAngle(rvpAngle){
+    lvpAngle.setValue(90 - rvpAngle);
+}
+
+function updateUniforms(config){
+    quadMaterial.uniforms.u_resolution.value = config.u_resolution;
+    quadMaterial.uniforms.u_hl.value = config.u_hl;
+    quadMaterial.uniforms.u_lineCount.value = config.u_lineCount;
+    quadMaterial.uniforms.u_lvp.value = config.u_lvp;
+    quadMaterial.uniforms.u_rvp.value = config.u_rvp;
+    quadMaterial.uniforms.u_lineThickness.value = config.u_lineThickness;
 }
 
 function render(){
-    updateUniforms();
+    const FOV = degToRad(controls.FOV);
+    const rvpAngle = degToRad(controls.rvpAngle);
+    const lvpAngle = degToRad(90 - controls.rvpAngle);
+    const spLength = getSPLength(FOV);
+
+    //.5 is the normalized center of the frame, since 0 and 1 are the edges
+    const RVP = .5 + getVPDistanceFromCenter(spLength, rvpAngle);
+    const LVP = .5 - getVPDistanceFromCenter(spLength, lvpAngle);
+
+    updateUniforms({
+        u_resolution: new THREE.Vector2(window.innerWidth, window.innerHeight),
+        u_hl: controls.horizon,
+        u_lineCount: controls.lineCount,
+        u_lvp: LVP,
+        u_rvp: RVP,
+        u_lineThickness: controls.lineThickness
+    });
+    setLVPAngle(controls.rvpAngle);
+    // console.log(controls.lvpAngle);
     renderer.render(scene, camera);
     requestAnimationFrame(render);
 }
